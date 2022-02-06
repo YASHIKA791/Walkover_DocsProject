4ae50800ef1b1fd13e1bf20d3c19c378e195b11b
Yashika <mails.yashika@gmail.com>
2022-02-06 21:14:30 +0530

    Create new workspace page created,routes changed

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
 
-
+var workspaces=[];
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 
@@ -20,14 +20,22 @@ app.use(express.static("public"));
 // ================================================
 
 
-app.get("/", function(req, res) {
+app.get("/login", function(req, res) {
   res.render("login"); 
 })
-
+app.get("/", function(req, res) {
+  res.render("create");
+});
 app.get("/sign-up", function(req, res) {
   res.render("signup"); 
 })
-
+app.get("/create-workspace", function(req, res) {
+  res.render("create"); 
+})
+app.post("/", function(req, res) {
+  
+  res.redirect("/");
+});
 
 // =================================================
 
