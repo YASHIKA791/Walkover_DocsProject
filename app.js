393ba2c4b50466749c3c3d40c61b1389602b0dbe
Yashika <mails.yashika@gmail.com>
2022-02-07 19:47:26 +0530

    Firebase Authentication added to login and signup pages

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//jshint esversion:6
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
+//const _ = require('lodash');
 
 var workspaces=[];
 
