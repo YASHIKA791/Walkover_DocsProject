2c4b1748637c0476ffd8189805f2d0bea44898f2
Yashika <mails.yashika@gmail.com>
2022-02-10 13:39:04 +0530

    Final Commit with all neccesary changes

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-//const _ = require('lodash');
 
-var workspaces=[];
 
+var workspaces = [];
+var files = [];
+var userEmail;
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-
+//app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
@@ -20,28 +21,69 @@ app.use(express.static("public"));
 // ================================================
 
 
-app.get("/login", function(req, res) {
-  res.render("login"); 
+app.get("/login", function (req, res) {
+  res.render("login");
 })
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("create");
 });
-app.get("/sign-up", function(req, res) {
-  res.render("signup"); 
+app.get("/sign-up", function (req, res) {
+  res.render("signup");
 })
-app.get("/create-workspace", function(req, res) {
-  res.render("create"); 
+app.get("/create-workspace", function (req, res) {
+  res.render("create");
 })
-app.post("/", function(req, res) {
-  
-  res.redirect("/");
+
+app.get("/workspace", function (req, res) {
+  res.render("workspace", { files: files });
+})
+app.get("/dashboard", function (req, res) {
+  // console.log(userName);
+  res.render("dashboard", { username: userEmail, workspaces_list: workspaces });
+})
+app.get("/workspace/:file", function (req, res) {
+  const requestedFile = req.params.file;
+  for (let i = 0; i < files.length; i++) {
+    if (files[i].name === requestedFile) {
+      res.render("showFile", { files: files, name: files[i].name, content: files[i].content })
+    }
+  }
+  // res.render("")
+})
+app.post("/dashboard", function (req, res) {
+  workspaces.push(req.body.new_workspace);
+  res.redirect("/dashboard");
 });
 
+app.post("/", function (req, res) {
+
+  res.redirect("/");
+});
+app.post("/api/register", function (req, res) {
+  console.log(req.body);
+  res.json({ status: 'ok' })
+})
+app.post("/api/login", function (req, res) {
+  // console.log(req.body.email);
+  userEmail = req.body.email;
+  res.json({ status: 'ok' })
+ 
+})
+app.post("/api/saveFileAndContent", function (req, res) {
+  let filename = req.body.filename;
+  let filecontent = req.body.filecontent;
+  let obj = {
+    name: filename,
+    content: filecontent
+  };
+  files.push(obj);
+  res.redirect("/workspace");
+})
 // =================================================
 
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
