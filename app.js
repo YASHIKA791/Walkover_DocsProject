3d57e480251200be6af8d819a5d33c312ec7d970
Yashika <mails.yashika@gmail.com>
2022-02-10 19:39:53 +0530

    app.js changed for heroku CI/CD deployment

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.post("/api/saveFileAndContent", function (req, res) {
 
 
 
-app.listen(3000, function () {
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
